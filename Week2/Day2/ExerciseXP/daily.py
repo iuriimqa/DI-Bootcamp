e4c7a9f62d47dfc9c7967d78864f1513fba60b35
iuriimcqa <iuriimcqa@gmail.com>
2023-03-28 17:24:13 +0300

    day3

--- Week2/Day2/ExerciseXP/daily.py
+++ Week2/Day2/ExerciseXP/daily.py
@@ -1,7 +1,23 @@
 # Daily1
 # Ask the user for a number and a length.
 # Create a program that prints a list of multiples of the number until the list length reaches length.
+#
+# a = input("Input number ")
+# b = input("Input length ")
+# a = int(a)
+# b = int(b)
+# list1 = []
+# for i in range(1,b+1):
+#     list1.append(i*a)
+# print(list1)
 
-a = input("Input number ")
-b = input("Input length ")
-while 
+# # Daily2
+# # Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+# strng2 = ''
+# word = input("Word ?")
+# for i in word:
+#     if len(strng2) == 0 or i != strng2[-1]:
+#         strng2 = strng2 + i
+#
+# print(strng2)
+
