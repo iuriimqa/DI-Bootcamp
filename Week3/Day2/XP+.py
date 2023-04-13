f1831e5c7ba71be70f441a8a6e0a201260350e22
iuriimcqa <iuriimcqa@gmail.com>
2023-04-13 18:13:14 +0300

    day4

--- Week3/Day2/XP+.py
+++ Week3/Day2/XP+.py
@@ -1,41 +1,68 @@
-# # # Daily1
-# # Create a class called Family and implement the following attributes:
-# #
-# # members: list of dictionaries with the following keys : name, age, gender and is_child (boolean).
-# # last_name : (string)
-# # Initial members data:
-# class Family:
-#
-#     def __init__(self,last_name, members):
-#         self.last_name = last_name
-#         self.members = members
-#
-#     def born(self,**kwargs):
-#             self.members.append(kwargs)
-#             print("Congratulations on the birth of a new family member: ",
-#               kwargs['name'])
-#
-#     def is_18(self, name):
-#             return ([n['age'] for n in members if n['name'] == name][0] > 18)
-#
-#     def family_presentation(self):
-#      print(f"Member's of Family: ")
-#
-#      for p in members:
-#             print(p['name'])
-#
-#
-# members = [
-#     {'name':'Michael','age':35,'gender':'Male','is_child':False},
-#     {'name':'Sarah','age':32,'gender':'Female','is_child':False}
-# ]
-#
-# family2 = Family('Stivenson',members)
-# boy = {'name':'Max','age':10,'gender':'Male','is_child':True}
-# family2.born(**boy)
-#
-# print(members)
-# print(Family.is_18('Stivenson','Michael'))
-# print(Family.family_presentation('Stivenson'))
+# Daily1 Family
+class Family():
+    def __init__(self):
+        self.members_key = ['self', 'name', 'age', 'gender', 'is_child']
+        self.members = [{'name': 'Michael', 'age': 35, 'gender': 'Male', 'is_child': False},
+                        {'name': 'Sarah', 'age': 32, 'gender': 'Female', 'is_child': False}]
+        self.last_name = 'Jackson'
 
-Daily2
+    def born(self, **kwargs):  # add new member
+        new_mf = dict(m for m in kwargs.items())
+        if "name" in new_mf.keys():
+            print("Congrats with a new member of family: ", new_mf['name'])
+            self.members.append(new_mf)
+            print(self.members)
+        else:
+            print('No new member in your family')
+
+    def is_18(self, name):
+        return ([n['age'] for n in self.members if n['name'] == name][
+                    0] > 18)  # it's a condition, it's ok to return a val
+
+    def family_presentation(self):
+        print(f'members of {self.last_name}: ')
+        for p in self.members:
+            print(p['name'])
+
+
+k = Family()
+k.born(name='Jacson', age=5, gender='male')
+print(k.is_18('Sarah'))  # check, is Sarah is older than 18 or not
+k.family_presentation()
+
+
+#Daily2 Incredibles
+class TheIncredibles(Family):
+    def __init__(self):
+        super().__init__()
+        self.members = [{'name': 'Michael', 'age': 35, 'gender': 'Male', 'is_child': False, 'power': 'fly',
+                         'incredible_name': 'MikeFly'},
+                        {'name': 'Sarah', 'age': 32, 'gender': 'Female', 'is_child': False, 'power': 'read minds',
+                         'incredible_name': 'SuperWoman'}]
+
+    def use_power(self, name):
+        if self.is_18(name):
+            d = [n['power'] for n in self.members if n['name'] == name]
+            print([n['power'] for n in self.members if n['name'] == name][0])
+
+    def incredible_presentation(self):
+        super().family_presentation()
+        print(f'members of {self.last_name}: ')
+        for p in self.members:
+            print(f"{p['name']} + {p['power']}")
+
+    def born(self, **kwargs):
+        new_mf = dict(m for m in kwargs.items())
+        if "name" in new_mf.keys():
+            print("Congrats with a new member of your family: ", new_mf['name'])
+            self.members.append(new_mf)
+            print(self.members)
+        else:
+            print('No new member in your family')
+
+
+b = TheIncredibles()
+b.use_power('Michael')
+b.incredible_presentation()
+b.born(name='Baby Jack', power='Unknown Power')
+b.incredible_presentation()
--- Week3/Day2/daily.py
+++ Week3/Day2/daily.py
@@ -2,64 +2,82 @@ import math
 
 
 class Pagination:
+    def __init__(self, items=[], pageSize=10) -> None:
+        self.pageSize = int(math.trunc(pageSize))
+        self.items = items
+        self.totalPages = 0
+        self.currentPage = 1
+        self.totalPages = (len(items)/math.ceil(pageSize))
+        self.totalPages = int(self.totalPages if self.totalPages == math.trunc(
+            self.totalPages) else self.totalPages+1)
+        self.currentDisplayItems = items[0:pageSize]
 
-    def __init__(self, items: list = None, page_size: int = 10) -> None:
-        if items is not None:
-            self.items = items
+    def getVisibleItems(self):
+        return (self.currentDisplayItems)
+
+    def prevPage(self):
+        if self.currentPage > 1:
+            self.currentPage -= 1
+            self.currentDisplayItems = self.items[(
+                self.currentPage-1)*self.pageSize:(self.currentPage)*self.pageSize]
+            return self
+
+    def nextPage(self):
+        if self.currentPage < self.totalPages:
+            self.currentPage += 1
+            self.currentDisplayItems = self.items[(
+                self.currentPage-1)*self.pageSize:self.currentPage*self.pageSize]
+            return self
+
+    def firstPage(self):
+        self.currentPage = 1
+        self.currentDisplayItems = self.items[(
+            self.currentPage-1)*self.pageSize:self.currentPage*self.pageSize]
+        return self
+
+    def lastPage(self):
+        self.currentPage = self.totalPages
+        self.currentDisplayItems = self.items[(
+            self.currentPage-1)*self.pageSize:self.currentPage*self.pageSize]
+        return self
+
+    def goToPage(self, pageNum):
+        if pageNum < 1:
+            self.currentPage=1
+        elif pageNum > self.totalPages:
+            self.currentPage=self.totalPages
         else:
-            self.items = []
-        self.page_size = page_size
-        self.current_idx = 0
-        self.total_pages = math.ceil(len(self.items) / self.page_size)
-
-    def get_visible_items(self):
-        start_idx = self.current_idx * self.page_size
-        end_idx = start_idx + self.page_size
-        return self.items[start_idx: end_idx]
-
-    def go_to_page(self, page_num: int):
-
-        if page_num > self.total_pages or page_num < 1:
-            raise ValueError(f"Incorrect page number - {page_num}. Available - {self.total_pages}")
-
-        self.current_idx = page_num - 1
-
-    def first_page(self):
-        self.go_to_page(1)
-        return self
-
-    def last_page(self):
-        self.go_to_page(self.total_pages)
-        return self
-
-    def next_page(self):
-        self.go_to_page(self.current_idx + 2)
-        return self
-
-    def previous_page(self):
-        self.go_to_page(self.current_idx)
-        return self
-
-    # dunder method - double underscore under method
-    def __str__(self) -> str:
-        out = ""
-        visible_items = self.get_visible_items()
-        for item in visible_items:
-            out += f'{item}\n'
-        return out
+            self.currentPage = pageNum
+        self.currentDisplayItems = self.items[(
+            self.currentPage-1)*self.pageSize:self.currentPage*self.pageSize]
 
 
-alphabetList = list("abcdefghijklmnopqrstuvwxyz")
 
-p = Pagination()
+alphabetList = [n for n in "abcdefghijklmnopqrstuvwxyz"]
 
-print(p.items)
+p = Pagination(alphabetList, 4)
 
-p.items.extend(alphabetList)
-p = Pagination()
+print(p.currentDisplayItems)
 
-print(p.items)
+print(p.getVisibleItems())
+print(p.pageSize, p.totalPages)
+p.nextPage()
+print(p.getVisibleItems())
+p.nextPage()
+print(p.getVisibleItems())
+p.nextPage()
+print(p.getVisibleItems())
 
-p_str = str(p)
+p.nextPage().nextPage()
+print(p.getVisibleItems())
+p.firstPage()
+print(p.getVisibleItems())
+p.lastPage()
+print(p.getVisibleItems())
+p.goToPage(0)
+print(p.getVisibleItems())
+p.goToPage(2)
+print(p.getVisibleItems())
+p.goToPage(20)
+print(p.getVisibleItems())
 
-print(p_str)
